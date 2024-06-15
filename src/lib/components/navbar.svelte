12164eca68bd150136ee36baaaffcd40aa70e5fc
max <29782397+tekoh@users.noreply.github.com>
2024-06-15 19:17:22 +0100

    dashboard layout

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,12 +1,22 @@
 <script>
+  import { page } from "$app/stores";
   import { auth } from "$lib/localAuth";
-  import { CircleUser } from "lucide-svelte";
+  import { AlignLeft, CircleUser } from "lucide-svelte";
 </script>
 
 <header class="flex w-full justify-center">
   <div class="navbar bg-base-200 lg:mt-3 lg:max-w-6xl lg:rounded-xl">
     <div class="flex-1">
-      <a href="/" class="btn btn-ghost text-xl font-semibold text-primary">store</a>
+      {#if $page.url.pathname.startsWith("/dashboard")}
+        <label for="my-drawer" class="btn btn-ghost drawer-button text-primary lg:hidden">
+          <AlignLeft strokeWidth={2.5} />
+        </label>
+        <a href="/" class="btn btn-ghost hidden text-xl font-semibold text-primary lg:inline-flex"
+          >store</a
+        >
+      {:else}
+        <a href="/" class="btn btn-ghost text-xl font-semibold text-primary">store</a>
+      {/if}
     </div>
     <div class="flex-none">
       {#if !$auth}
