ca2c500b08701f9c4c92a36d336d02758ec78dcd
max <29782397+mxz7@users.noreply.github.com>
2024-06-25 14:09:23 +0100

    show auth required prompt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+  import { auth } from "$lib/stores";
+
+  $: console.log($auth);
 </script>
 
 <svelte:head>
@@ -13,5 +16,17 @@
 <p class="text-center text-sm italic text-secondary">private file storage</p>
 
 <div class="mt-8 flex w-full justify-center">
-  <div class="w-full px-4 md:max-w-4xl md:px-0"></div>
+  <div class="w-full px-4 md:max-w-4xl md:px-0">
+    {#if !$auth}
+      <div class="flex w-full justify-center">
+        <span class="loading loading-spinner loading-lg"></span>
+      </div>
+    {:else if $auth && !$auth.authenticated}
+      <h3 class="text-center text-error">You must be logged in to upload files</h3>
+      <div class="mt-2 flex w-full justify-center gap-4">
+        <a href="/login" class="btn">Log in</a>
+        <a href="/signup" class="btn">Sign up</a>
+      </div>
+    {/if}
+  </div>
 </div>
