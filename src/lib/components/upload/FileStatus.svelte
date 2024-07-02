cefd82c5e915133d49d9730fb6f219ed61b99c6a
max <29782397+mxz7@users.noreply.github.com>
2024-07-03 00:22:22 +0100

    toast on copy

--- src/lib/components/upload/FileStatus.svelte
+++ src/lib/components/upload/FileStatus.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { formatBytes } from "$lib/format";
   import { Check, CircleX, Copy } from "lucide-svelte";
+  import toast from "svelte-french-toast";
   import { fly } from "svelte/transition";
   import CopyClipboard from "../CopyClipboard.svelte";
   import type { FileData } from "./file";
@@ -16,6 +17,15 @@
     });
 
     component.$destroy();
+
+    toast.success("Copied to your clipboard", {
+      style:
+        "--tw-bg-opacity: 1; background-color: var(--fallback-b3,oklch(var(--b3)/var(--tw-bg-opacity))); --tw-text-opacity: 1; color: var(--fallback-bc,oklch(var(--bc)/var(--tw-text-opacity)));",
+      iconTheme: {
+        primary: "#a6e3a1",
+        secondary: "#FFFFFF",
+      },
+    });
   }
 </script>
 
